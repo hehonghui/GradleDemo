858e14baf5812950baa49381f2270b413b9a8fe8
mr.simple <bboyfeiyu@gmail.com>
2016-08-01 20:06:25 +0800

    update

--- app/src/main/java/com/simple/gradleplugin/SDKManager.java
+++ app/src/main/java/com/simple/gradleplugin/SDKManager.java
@@ -4,10 +4,10 @@ package com.simple.gradleplugin;
  * Created by mrsimple on 28/7/16.
  */
 public class SDKManager {
-    private static final SDKManager INSTANCCE = new SDKManager();
+    private static final SDKManager S_INSTANCE = new SDKManager();
 
     public static SDKManager getInstance() {
-        return INSTANCCE;
+        return S_INSTANCE;
     }
 
     private SDKManager() {
